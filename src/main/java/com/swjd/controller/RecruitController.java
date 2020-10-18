7be4796f44846436f1609b7155911f6c536db3ce
Administrator <1151788973@qq.com>
2020-10-18 22:16:17 +0800

    Recruit招聘信息表

--- src/main/java/com/swjd/controller/RecruitController.java
+++ src/main/java/com/swjd/controller/RecruitController.java
@@ -25,14 +25,21 @@ public class RecruitController {
     @Autowired
     private RecruitService recruitService;
 
-    // 查询所有招聘信息(分页)
+    /**
+     * 查询招聘信息
+     * @return
+     */
     @GetMapping("/getAllRecruit")
     public R getAllRecruit() {
         List<Recruit> allRecruit = recruitService.getAllRecruit();
         return allRecruit!=null?R.ok().put("data",allRecruit):R.error("查询所有招聘信息失败");
     }
 
-    // 添加招聘信息
+    /**
+     * 添加招聘信息
+     * @param recruit
+     * @return
+     */
     @PostMapping("/addRecruit")
     public R addRecruit(Recruit recruit) {
 
@@ -43,7 +50,11 @@ public class RecruitController {
         return size>0?R.ok("添加招聘信息成功"):R.error("添加招聘信息失败");
     }
 
-    // 根据岗位名查询招聘信息
+    /**
+     * 根据岗位名查询招聘信息
+     * @param name
+     * @return
+     */
     @GetMapping("/getByNameRecruit/{name}")
     public R getByNameRecruit(@PathVariable("name") String name) {
         if (name == null) {
@@ -53,7 +64,11 @@ public class RecruitController {
         return recruits!=null?R.ok().put("data",recruits):R.error("根据名字查询招聘信息失败");
     }
 
-    // 修改招聘信息
+    /**
+     * 修改招聘信息
+     * @param recruit
+     * @return
+     */
     @PostMapping("/updateRecruit")
     public R updateRecruit(Recruit recruit) {
         if (recruit.getId() == null) {
@@ -63,7 +78,11 @@ public class RecruitController {
         return size>0?R.ok():R.error("修改招聘信息失败");
     }
 
-    // 删除招聘信息
+    /**
+     * 删除招聘信息
+     * @param id
+     * @return
+     */
     @GetMapping("/delRecruit/{id}")
     public R delRecruit(@PathVariable("id") Integer id) {
         if (id == null) {
